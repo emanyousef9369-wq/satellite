86a441de5b18c7ca832edf9b4c2dda923bc910d6
emanyousef9369-wq <emanyousef9369@gmail.com>
2026-05-01 03:58:18 +0300

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,31 @@
 import streamlit as st
+import numpy as np
+import tensorflow as tf
+from PIL import Image
 
-st.title("Test App 🚀")
-st.write("If you see this, app is working!")
+
+model = tf.keras.models.load_model("final_model.keras")
+
+classes = ['cloudy', 'fire', 'floods', 'normal']
+
+st.title("🌍 Satellite Image Classification App")
+st.write("Upload a satellite image and get prediction")
+
+
+uploaded_file = st.file_uploader("Choose an image", type=["jpg", "png", "jpeg"])
+
+def preprocess(img):
+    img = img.resize((64, 64))
+    img = np.array(img) / 255.0
+    img = np.expand_dims(img, axis=0)
+    return img
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
+
+    img_array = preprocess(image)
+    prediction = model.predict(img_array)
+    class_index = np.argmax(prediction)
+    
+    st.success(f"Prediction: {classes[class_index]}")
